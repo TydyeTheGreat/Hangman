6fa412a025fc1cb429e88f16e6c66dab7acc499f
Tyler Swartz <ssdralian@gmail.com>
2017-10-07 17:58:21 +0000

    pls

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 
 // Variables
 
+var config = {
+    apiKey: "AIzaSyB-v7qSWUBWKaUigrkWkwCjxtkmmmOcDFY",
+    authDomain: "hangman-69a47.firebaseapp.com",
+    databaseURL: "hangman-69a47:.firebaseio.com",
+    storageBucket: "gs://hangman-69a47.appspot.com",
+  };
+  
+firebase.initializeApp(config);
 
+const myModule = require('./words');
+let val = myModule.hello();
 
 // Detect if User Signed In
 
 $(document).ready(function(){
-    
+    console.log("ready");
+});
+
+$(".singleplayer").on("click", function(){
+   
 });
